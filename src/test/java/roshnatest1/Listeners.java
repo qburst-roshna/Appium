040f7d420089b193eb8a9fee10d670193bea89a0
qburst-roshna <roshna@qburst.com>
2023-07-18 11:55:47 +0100

    added listneners class

---
+++ src/test/java/roshnatest1/Listeners.java
@@ -0,0 +1,30 @@
+package roshnatest1;
+
+import java.time.Duration;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.RemoteWebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.ITestListener;
+import org.testng.annotations.Test;
+import org.testng.internal.annotations.IListeners;
+
+import com.google.common.collect.ImmutableMap;
+
+import io.appium.java_client.AppiumBy;
+import io.appium.java_client.android.nativekey.AndroidKey;
+import io.appium.java_client.android.nativekey.KeyEvent;
+
+public  class Listeners implements ITestListener{
+	
+	
+}
